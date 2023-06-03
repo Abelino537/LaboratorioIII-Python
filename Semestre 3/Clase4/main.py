50a82c22534e23f1e3338ea9aee3dae13f80c0a0
Abel Pierna <103205401+Abelino537@users.noreply.github.com>
2023-06-03 17:49:26 -0300

    Abel Commit Adicion
    agrego la clase 4 de python donde comenzamos a ver la conexion con la base de datos postgres

---
+++ Semestre 3/Clase4/main.py
@@ -0,0 +1,24 @@
+"""
+Laboratorio III
+Clase 4
+Abel Pierna
+24/04/23
+conexión a base de datos
+"""
+import psycopg2
+
+connection = psycopg2.connect(
+    user='postgres',
+    password='admin',
+    host='127.0.0.1',
+    port='5432',
+    database='test_bd'
+)
+cursor = connection.cursor()
+sentencia = 'SELECT * FROM persona '
+cursor.execute(sentencia)
+registro = cursor.fetchall()
+for row in registro:
+    print(row)
+cursor.close()
+connection.close()
